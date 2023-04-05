c1d66bffb5b13a2e63ca2a756d948b150d06fb14
danmarvarela <danmarvarela@macs-MacBook-Pro.local>
2023-04-05 15:51:36 +0800

    Escape apostrophe

--- src/pages/about.tsx
+++
@@ -1,5 +0,0 @@
-export default function Projects() {
-  return (
-    <div>This is the about page</div>
-  )
-}
--- src/pages/projects.tsx
+++
@@ -1,5 +0,0 @@
-export default function Projects() {
-  return (
-    <div>This is the projects page</div>
-  )
-}
