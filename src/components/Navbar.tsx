408290b18a4ed60af1b12e77e39e221bd4e03f5e
danmarvarela <danmarvarela@macs-MacBook-Pro.local>
2023-04-07 05:15:11 +0800

    Change logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="w-screen border-gray-300 h-16 flex justify-center fixed top-0 left-0 bg-white/30 backdrop-blur-sm">
       <div className="w-full max-w-lg sm:max-w-xl md:max-w-3xl h-full flex items-center justify-between px-4">
-        <Image src="/images/logo.png" alt="logo" width={40} height={40} />
+        <Image src="/images/logo.svg" alt="logo" width={40} height={40} />
         <Socials />
       </div>
     </nav>
