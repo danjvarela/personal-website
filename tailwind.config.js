a02ff7bcda4d7fa820efb791ca6a2a71b676203e
danmarvarela <danmarvarela@macs-MacBook-Pro.local>
2023-04-07 04:48:23 +0800

    Remove tw-elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,12 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
-    require("tw-elements/dist/plugin")
   ],
 }
 
