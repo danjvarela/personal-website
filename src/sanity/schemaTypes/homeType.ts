8d4d4811e87bd2eaf6c228f33c73ddaa95c3a9e9
Dan Varela <vareladanmarj@gmail.com>
2024-08-21 00:03:24 +0800

    fix: only allow certain block elements on home page content field

--- src/sanity/schemaTypes/homeType.ts
+++ src/sanity/schemaTypes/homeType.ts
@@ -13,6 +13,12 @@ export const homeType = defineType({
         defineArrayMember({
           type: "block",
           of: [defineArrayMember({ type: "linkWithIcon" })],
+          styles: [
+            { title: "Normal", value: "normal" },
+            { title: "H3", value: "h3" },
+            { title: "H4", value: "h4" },
+            { title: "Quote", value: "blockquote" },
+          ],
         }),
         defineArrayMember({ type: "linkWithDescription" }),
       ],
