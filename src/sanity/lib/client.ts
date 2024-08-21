0e6a7f009366cc5f75a51d14a4d08795bc466a57
Dan Varela <vareladanmarj@gmail.com>
2024-08-21 19:52:55 +0800

    feat: shorten revalidation time during development

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -19,7 +19,7 @@ export async function sanityFetch<QueryResponse>({
 }) {
   return client.fetch<QueryResponse>(query, params, {
     next: {
-      revalidate: process.env.NODE_ENV === "development" ? 30 : 3600,
+      revalidate: process.env.NODE_ENV === "development" ? 10 : 3600,
       tags,
     },
   })
