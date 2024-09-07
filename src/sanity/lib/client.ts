d37e0c1dcfe0dce495c1284137dbb1af0583a173
Dan Varela <vareladanmarj@gmail.com>
2024-09-08 02:19:13 +0800

    fix: revalidation not set properly

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -6,7 +6,7 @@ export const client = createClient({
   projectId: env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: env.NEXT_PUBLIC_SANITY_API_VERSION,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: false,
   stega: {
     enabled: process.env.NEXT_PUBLIC_VERCEL_ENV === "preview",
     studioUrl: "/studio",
@@ -26,21 +26,23 @@ export async function sanityFetch<QueryResponse>({
 }) {
   const isDraftMode = draftMode().isEnabled
 
-  let dynamicRevalidate = revalidate
+  const dynamicRevalidate = (() => {
+    // we don't cache when in draft mode
+    if (isDraftMode) return 0
 
-  if (isDraftMode) {
-    // Do not cache in Draft Mode
-    dynamicRevalidate = 0
-  } else if (tags?.length) {
-    // Cache indefinitely if tags supplied, purge with revalidateTag()
-    dynamicRevalidate = false
-  }
+    // if there is a tag supplied, cache indefinitely until revalidateTag is called
+    if (tags?.length) return false
+
+    if (revalidate !== undefined) return revalidate
+
+    return process.env.NODE_ENV === "development" ? 10 : 3600
+  })()
 
   const draftModeOpts: QueryOptions = isDraftMode
     ? {
         token: env.SANITY_API_READ_TOKEN,
         perspective: "previewDrafts",
-        // stega: true,
+        stega: true,
         useCdn: false,
       }
     : {}
@@ -48,7 +50,7 @@ export async function sanityFetch<QueryResponse>({
   return client.fetch<QueryResponse>(query, params, {
     ...draftModeOpts,
     next: {
-      revalidate: process.env.NODE_ENV === "development" ? 10 : 3600,
+      revalidate: dynamicRevalidate,
       tags,
     },
   })
